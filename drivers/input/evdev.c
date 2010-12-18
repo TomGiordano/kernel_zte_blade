e5b6536f8cf84bb9120986e7326075696972adf4
Henrik Rydberg <rydberg@euromail.se>
2010-12-18 20:51:13 +0100

    Input: introduce device properties
    Today, userspace sets up an input device based on the data it emits.
    This is not always enough; a tablet and a touchscreen may emit exactly
    the same data, for instance, but the former should be set up with a
    pointer whereas the latter does not need to. Recently, a new type of
    touchpad has emerged where the buttons are under the pad, which
    changes logic without changing the emitted data. This patch introduces
    a new ioctl, EVIOCGPROP, which enables user access to a set of device
    properties useful during setup. The properties are given as a bitmap
    in the same fashion as the event types, and are also made available
    via sysfs, uevent and /proc/bus/input/devices.
    
    Acked-by: Ping Cheng <pingc@wacom.com>
    Acked-by: Chase Douglas <chase.douglas@canonical.com>
    Acked-by: Dmitry Torokhov <dtor@mail.ru>
    Signed-off-by: Henrik Rydberg <rydberg@euromail.se>

--- drivers/input/evdev.c
+++ drivers/input/evdev.c
@@ -530,6 +530,7 @@ static long evdev_do_ioctl(struct file *file, unsigned int cmd,
 	struct ff_effect effect;
 	int __user *ip = (int __user *)p;
 	unsigned int i, t, u, v;
+        unsigned int size;
 	int error;
 
 	switch (cmd) {
@@ -598,8 +599,16 @@ static long evdev_do_ioctl(struct file *file, unsigned int cmd,
 			return evdev_grab(evdev, client);
 		else
 			return evdev_ungrab(evdev, client);
+        }               
+        size = _IOC_SIZE(cmd);
+#define EVIOC_MASK_SIZE(nr)	((nr) & ~(_IOC_SIZEMASK << _IOC_SIZESHIFT))
+	switch (EVIOC_MASK_SIZE(cmd)) {
 
-	default:
+	case EVIOCGPROP(0):
+		return bits_to_user(dev->propbit, INPUT_PROP_MAX,
+				    size, p, compat_mode);
+        }
+//	default:
 
 		if (_IOC_TYPE(cmd) != 'E')
 			return -EINVAL;
@@ -703,7 +712,7 @@ static long evdev_do_ioctl(struct file *file, unsigned int cmd,
 				return 0;
 			}
 		}
-	}
+//	}
 	return -EINVAL;
 }
 
