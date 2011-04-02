d7b4edcb685dc499bf360c188d8d3b546ec418a8
Jacob Crawley <jacobcrawley@gmail.com>
2011-04-02 17:27:57 -0400

    Add interactive CPU governor.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -341,6 +341,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS)
 extern struct cpufreq_governor cpufreq_gov_smartass;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
+extern struct cpufreq_governor cpufreq_gov_interactive;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_interactive)
 #endif
 
 
