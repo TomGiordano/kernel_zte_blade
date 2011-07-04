39cef6f41966d6de6e920fd6c530e1d40f77d2da
Tom Giordano <tomgiordano83@gmail.com>
2011-07-04 22:23:17 +1000

    fm_4708: fix initialise flag and don't power off until all fd closed
    Fix the initialised flag so it gets set & increament it as more fds are opened.
    
    Don't power off until all fds are closed.

--- drivers/misc/fm_si4708.c
+++ drivers/misc/fm_si4708.c
@@ -743,7 +743,8 @@ static int si4708_open(struct inode *inode, struct file *filp)
 	/*checkout if the device is open*/
 	if(fm_si4708_dev->initialized)
 	{
-	    	res = si4708_standby2normal();
+		res = si4708_standby2normal();
+		fm_si4708_dev->initialized++;
 	}
 	else
        {
@@ -761,7 +762,7 @@ static int si4708_open(struct inode *inode, struct file *filp)
 		spin_unlock_irq(&fm_si4708_dev->lock);
 
 		res = si4708_init2normal();
-		if( res > 0)
+		if( res >= 0)
 		      fm_si4708_dev->initialized++;
 	}
 
@@ -796,12 +797,15 @@ static int si4708_open(struct inode *inode, struct file *filp)
  **********************************************************************/
 static int si4708_release(struct inode *inode, struct file *filp)
 {
-	int res;
+	int res = 0;
 	/*set close status,flag = 0*/
-	fm_si4708_dev->initialized = 0;
-	/* Make sure the device is power down. */
-	res = si4708_normal2standby();
-
+	if(fm_si4708_dev->initialized)
+	    fm_si4708_dev->initialized--;
+	/* Only power down when all fd are closed */
+	if(!fm_si4708_dev->initialized){
+	    /* Make sure the device is power down. */
+	    res = si4708_normal2standby();
+	}
 	return res < 0 ? res : 0;
 }
 
