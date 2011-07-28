793789fcff35d51739615aee1425e3fd1bad2f21
Ravishangar Kalyanam <rkalya@codeaurora.org>
2011-07-28 16:49:25 -0700

    msm_fb: display: Add boot param LCDC/MIPI panel detection support
    Add boot param LCDC/MIPI panel detection support for automatically
    loading selected panels through fastboot command
    
    Signed-off-by: Ravishangar Kalyanam <rkalya@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/lcdc_auo_wvga.c
    	drivers/video/msm/lcdc_chimei_wxga.c
    	drivers/video/msm/lcdc_samsung_oled_pt.c
    	drivers/video/msm/lcdc_samsung_wsvga.c
    	drivers/video/msm/mipi_renesas_video_fwvga_pt.c
    	drivers/video/msm/mipi_simulator_video.c
    	drivers/video/msm/mipi_toshiba.c

--- drivers/video/msm/mipi_renesas_video_fwvga_pt.c
+++ drivers/video/msm/mipi_renesas_video_fwvga_pt.c
@@ -16,7 +16,7 @@
 
 static struct msm_panel_info pinfo;
 
-static struct mipi_dsi_phy_ctrl dsi_cmd_mode_phy_db = {
+static struct mipi_dsi_phy_ctrl dsi_video_mode_phy_db = {
 #ifdef CONFIG_FB_MSM_MDP303
 	/* DSI Bit Clock at 500 MHz, 2 lane, RGB888 */
 	/* regulator */
@@ -29,40 +29,42 @@ static struct mipi_dsi_phy_ctrl dsi_cmd_mode_phy_db = {
 	/* strength */
 	{0xbb, 0x02, 0x06, 0x00},
 	/* pll control */
-	{0x01, 0xec, 0x31, 0xd2, 0x00, 0x40, 0x37, 0x62,
+	{0x00, 0xec, 0x31, 0xd2, 0x00, 0x40, 0x37, 0x62,
 	0x01, 0x0f, 0x07,
 	0x05, 0x14, 0x03, 0x0, 0x0, 0x0, 0x20, 0x0, 0x02, 0x0},
 #else
 	/* DSI_BIT_CLK at 400MHz, 1 lane, RGB888 */
-	{0x03, 0x01, 0x01, 0x00},	/* regulator */
+	/* regulator */
+	{0x03, 0x01, 0x01, 0x00},
 	/* timing   */
-	{0x22, 0x0c, 0x7, 0x00, 0x10, 0x20, 0x10,
-	0xd, 0x8, 0x2, 0x3},
+	{0xaa, 0x3b, 0x1b, 0x00, 0x52, 0x58, 0x20, 0x3f,
+	0x2e, 0x03, 0x04},
 	/* phy ctrl */
 	{0x7f, 0x00, 0x00, 0x00},
 	/* strength */
-	{0xee, 0x00, 0x6, 0x00},
+	{0xee, 0x00, 0x86, 0x00},
 	/* pll control */
-	{0x40, 0x2f, 0xb1, 0xda, 0x00, 0x50, 0x48, 0x63,
+	{0x40, 0xc7, 0xb0, 0xda, 0x00, 0x50, 0x48, 0x63,
 #if defined(RENESAS_FWVGA_TWO_LANE)
-	0x33, 0x1f, 0x07,
-#else	/* default set to 1 lane */
+	0x30, 0x07, 0x03,
+#else
+	/* default set to 1 lane */
 	0x30, 0x07, 0x07,
 #endif
 	0x05, 0x14, 0x03, 0x0, 0x0, 0x54, 0x06, 0x10, 0x04, 0x0},
 #endif
 };
 
-static int __init mipi_cmd_renesas_fwvga_pt_init(void)
+static int __init mipi_video_renesas_fwvga_pt_init(void)
 {
 	int ret;
 
-	if (msm_fb_detect_client("mipi_cmd_renesas_fwvga"))
+	if (msm_fb_detect_client("mipi_video_renesas_fwvga"))
 		return 0;
 
 	pinfo.xres = 480;
 	pinfo.yres = 864;
-	pinfo.type = MIPI_CMD_PANEL;
+	pinfo.type = MIPI_VIDEO_PANEL;
 	pinfo.pdest = DISPLAY_1;
 	pinfo.wait_cycle = 0;
 	pinfo.bpp = 24;
@@ -73,23 +75,33 @@ static int __init mipi_cmd_renesas_fwvga_pt_init(void)
 	pinfo.lcdc.v_back_porch = 20;
 	pinfo.lcdc.v_front_porch = 20;
 	pinfo.lcdc.v_pulse_width = 1;
+	pinfo.clk_rate = 499000000;
 #else
-	pinfo.lcdc.h_front_porch = 50;
+
 #if defined(RENESAS_FWVGA_TWO_LANE)
 	pinfo.lcdc.h_back_porch = 400;
+#else
+	pinfo.lcdc.h_back_porch = 50;
+#endif
+	pinfo.lcdc.h_front_porch = 50;
+
+#if defined(RENESAS_FWVGA_TWO_LANE)
 	pinfo.lcdc.h_pulse_width = 5;
+#else
+	pinfo.lcdc.h_pulse_width = 20;
+#endif
+
+#if defined(RENESAS_FWVGA_TWO_LANE)
 	pinfo.lcdc.v_back_porch = 75;
 	pinfo.lcdc.v_front_porch = 5;
 	pinfo.lcdc.v_pulse_width = 1;
 #else
-	pinfo.lcdc.h_back_porch = 50;
-	pinfo.lcdc.h_pulse_width = 20;
 	pinfo.lcdc.v_back_porch = 10;
 	pinfo.lcdc.v_front_porch = 10;
 	pinfo.lcdc.v_pulse_width = 5;
 #endif
 
-#endif /* CONFIG_FB_MSM_MDP303 */
+#endif
 	pinfo.lcdc.border_clr = 0;	/* blk */
 	pinfo.lcdc.underflow_clr = 0xff;	/* blue */
 	pinfo.lcdc.hsync_skew = 0;
@@ -97,54 +109,48 @@ static int __init mipi_cmd_renesas_fwvga_pt_init(void)
 	pinfo.bl_min = 1;
 	pinfo.fb_num = 2;
 
+	pinfo.mipi.mode = DSI_VIDEO_MODE;
+	pinfo.mipi.pulse_mode_hsa_he = TRUE;
+	pinfo.mipi.hfp_power_stop = TRUE;
+	pinfo.mipi.hbp_power_stop = TRUE;
+	pinfo.mipi.hsa_power_stop = TRUE;
+	pinfo.mipi.eof_bllp_power_stop = TRUE;
+	pinfo.mipi.bllp_power_stop = TRUE;
 #ifdef CONFIG_FB_MSM_MDP303
-	pinfo.clk_rate = 499000000;
-#else
-	pinfo.clk_rate = 152000000;
-#endif
-	pinfo.lcd.refx100 = 6000; /* adjust refx100 to prevent tearing */
-
-	pinfo.mipi.mode = DSI_CMD_MODE;
-	pinfo.mipi.dst_format = DSI_CMD_DST_FORMAT_RGB888;
+	pinfo.mipi.traffic_mode = DSI_BURST_MODE;
+	pinfo.mipi.dst_format =  DSI_VIDEO_DST_FORMAT_RGB888;
 	pinfo.mipi.vc = 0;
 	pinfo.mipi.rgb_swap = DSI_RGB_SWAP_RGB;
 	pinfo.mipi.data_lane0 = TRUE;
-#ifdef CONFIG_FB_MSM_MDP303
-	pinfo.lcd.vsync_enable = TRUE;
-	pinfo.lcd.hw_vsync_mode = TRUE;
 	pinfo.mipi.data_lane1 = TRUE;
 	pinfo.mipi.t_clk_post = 0x20;
 	pinfo.mipi.t_clk_pre = 0x2F;
 	pinfo.mipi.stream = 0; /* dma_p */
-	pinfo.mipi.mdp_trigger = DSI_CMD_TRIGGER_SW;
+	pinfo.mipi.mdp_trigger = DSI_CMD_TRIGGER_NONE;
 	pinfo.mipi.dma_trigger = DSI_CMD_TRIGGER_SW;
-	pinfo.mipi.te_sel = 1; /* TE from vsync gpio */
-	pinfo.mipi.interleave_max = 1;
-	pinfo.mipi.insert_dcs_cmd = TRUE;
-	pinfo.mipi.wr_mem_continue = 0x3c;
-	pinfo.mipi.wr_mem_start = 0x2c;
-	pinfo.mipi.dsi_phy_db = &dsi_cmd_mode_phy_db;
-	pinfo.mipi.tx_eot_append = 0x01;
-	pinfo.mipi.rx_eot_ignore = 0;
+	pinfo.mipi.frame_rate = 60;
+	pinfo.mipi.dsi_phy_db = &dsi_video_mode_phy_db;
 	pinfo.mipi.dlane_swap = 0x01;
+	pinfo.mipi.tx_eot_append = 0x01;
 #else
+	pinfo.mipi.traffic_mode = DSI_NON_BURST_SYNCH_PULSE;
+	pinfo.mipi.dst_format = DSI_VIDEO_DST_FORMAT_RGB888;
+	pinfo.mipi.vc = 0;
+	pinfo.mipi.rgb_swap = DSI_RGB_SWAP_BGR;
+	pinfo.mipi.data_lane0 = TRUE;
 #if defined(RENESAS_FWVGA_TWO_LANE)
 	pinfo.mipi.data_lane1 = TRUE;
 #else
 	pinfo.mipi.data_lane1 = FALSE;
 #endif
-	pinfo.mipi.t_clk_post = 0x18;
-	pinfo.mipi.t_clk_pre = 0x14;
-	pinfo.mipi.stream = 0;	/* dma_p */
+	pinfo.mipi.t_clk_post = 0x03;
+	pinfo.mipi.t_clk_pre = 0x24;
+	pinfo.mipi.stream = 0; /* dma_p */
 	pinfo.mipi.mdp_trigger = DSI_CMD_TRIGGER_SW;
 	pinfo.mipi.dma_trigger = DSI_CMD_TRIGGER_SW;
-	pinfo.mipi.te_sel = 1; /* TE from vsycn gpio */
-	pinfo.mipi.interleave_max = 1;
-	pinfo.mipi.insert_dcs_cmd = TRUE;
-	pinfo.mipi.wr_mem_continue = 0x3c;
-	pinfo.mipi.wr_mem_start = 0x2c;
-	pinfo.mipi.dsi_phy_db = &dsi_cmd_mode_phy_db;
-#endif /* CONFIG_FB_MSM_MDP303 */
+	pinfo.mipi.frame_rate = 60;
+	pinfo.mipi.dsi_phy_db = &dsi_video_mode_phy_db;
+#endif
 
 	ret = mipi_renesas_device_register(&pinfo, MIPI_DSI_PRIM,
 						MIPI_DSI_PANEL_FWVGA_PT);
@@ -154,4 +160,4 @@ static int __init mipi_cmd_renesas_fwvga_pt_init(void)
 	return ret;
 }
 
-module_init(mipi_cmd_renesas_fwvga_pt_init);
+module_init(mipi_video_renesas_fwvga_pt_init);
--- drivers/video/msm/mipi_simulator_video.c
+++ drivers/video/msm/mipi_simulator_video.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2011, Code Aurora Forum. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -8,60 +8,51 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301, USA.
- *
  */
 
 #include "msm_fb.h"
 #include "mipi_dsi.h"
-#include "mipi_novatek.h"
+#include "mipi_simulator.h"
 
 static struct msm_panel_info pinfo;
 
 static struct mipi_dsi_phy_ctrl dsi_video_mode_phy_db = {
-/* DSI_BIT_CLK at 400MHz, 1 lane, RGB888 */
-		{0x03, 0x01, 0x01, 0x00},	/* regulator */
-		/* timing   */
-		{0x96, 0x26, 0x23, 0x0, 0x50, 0x4B, 0x1e,
-		0x28, 0x28, 0x03, 0x04},
-		{0x7f, 0x00, 0x00, 0x00},	/* phy ctrl */
-		{0xee, 0x02, 0x86, 0x00},	/* strength */
-		/* pll control */
-		{0x40, 0xf9, 0xb0, 0xda, 0x00, 0x50, 0x48, 0x63,
-		0x30, 0x07, 0x07,
+	{0x03, 0x01, 0x01, 0x00},
+	{0xaa, 0x3b, 0x1b, 0x00, 0x52, 0x58, 0x20, 0x3f,
+		0x2e, 0x03, 0x04},
+	{0x7f, 0x00, 0x00, 0x00},
+	{0xee, 0x00, 0x86, 0x00},
+	{0x40, 0xc7, 0xb0, 0xda, 0x00, 0x50, 0x48, 0x63,
+		0x30, 0x07, 0x03,
 		0x05, 0x14, 0x03, 0x0, 0x0, 0x54, 0x06, 0x10, 0x04, 0x0},
 };
 
-static int __init mipi_video_novatek_qhd_pt_init(void)
+static int __init mipi_video_simulator_init(void)
 {
 	int ret;
 
-	if (msm_fb_detect_client("mipi_video_novatek_qhd"))
+	if (msm_fb_detect_client("mipi_video_simulator_vga"))
 		return 0;
-
-	pinfo.xres = 540;
-	pinfo.yres = 960;
+	pinfo.xres = 640;
+	pinfo.yres = 480;
 	pinfo.type = MIPI_VIDEO_PANEL;
 	pinfo.pdest = DISPLAY_1;
 	pinfo.wait_cycle = 0;
 	pinfo.bpp = 24;
-	pinfo.lcdc.h_back_porch = 50;
-	pinfo.lcdc.h_front_porch = 50;
-	pinfo.lcdc.h_pulse_width = 20;
-	pinfo.lcdc.v_back_porch = 11;
-	pinfo.lcdc.v_front_porch = 10;
-	pinfo.lcdc.v_pulse_width = 5;
+
+	pinfo.lcdc.h_back_porch  = 6;
+	pinfo.lcdc.h_front_porch = 6;
+	pinfo.lcdc.h_pulse_width = 2;
+	pinfo.lcdc.v_back_porch  = 6;
+	pinfo.lcdc.v_front_porch = 6;
+	pinfo.lcdc.v_pulse_width = 2;
+
 	pinfo.lcdc.border_clr = 0;	/* blk */
 	pinfo.lcdc.underflow_clr = 0xff;	/* blue */
 	pinfo.lcdc.hsync_skew = 0;
 	pinfo.bl_max = 15;
 	pinfo.bl_min = 1;
 	pinfo.fb_num = 2;
-	pinfo.clk_rate = 500000000;
 
 	pinfo.mipi.mode = DSI_VIDEO_MODE;
 	pinfo.mipi.pulse_mode_hsa_he = TRUE;
@@ -73,22 +64,23 @@ static int __init mipi_video_novatek_qhd_pt_init(void)
 	pinfo.mipi.traffic_mode = DSI_NON_BURST_SYNCH_PULSE;
 	pinfo.mipi.dst_format = DSI_VIDEO_DST_FORMAT_RGB888;
 	pinfo.mipi.vc = 0;
-	pinfo.mipi.rgb_swap = DSI_RGB_SWAP_BGR;
+	pinfo.mipi.rgb_swap = DSI_RGB_SWAP_RGB;
 	pinfo.mipi.data_lane0 = TRUE;
-	pinfo.mipi.tx_eot_append = TRUE;
-	pinfo.mipi.t_clk_post = 10;
-	pinfo.mipi.t_clk_pre = 30;
+	pinfo.mipi.data_lane1 = TRUE;
+	pinfo.mipi.t_clk_post = 0x03;
+	pinfo.mipi.t_clk_pre = 0x24;
 	pinfo.mipi.stream = 0; /* dma_p */
 	pinfo.mipi.mdp_trigger = DSI_CMD_TRIGGER_SW;
 	pinfo.mipi.dma_trigger = DSI_CMD_TRIGGER_SW;
+	pinfo.mipi.frame_rate = 60;
 	pinfo.mipi.dsi_phy_db = &dsi_video_mode_phy_db;
 
-	ret = mipi_novatek_device_register(&pinfo, MIPI_DSI_PRIM,
-						MIPI_DSI_PANEL_WVGA_PT);
+	ret = mipi_simulator_device_register(&pinfo, MIPI_DSI_PRIM,
+		MIPI_DSI_PANEL_FWVGA_PT);
 	if (ret)
 		pr_err("%s: failed to register device!\n", __func__);
 
-	return ret;
+		return ret;
 }
 
-module_init(mipi_video_novatek_qhd_pt_init);
+module_init(mipi_video_simulator_init);
