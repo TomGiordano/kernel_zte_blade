59db26cc664cf0c49451f011325cb231398f891e
Laura Abbott <lauraa@codeaurora.org>
2011-08-19 13:36:32 -0700

    gpu: ion: Add support for carveout heaps on msm targets
    Add infrastructure for supporting ion carveout heaps.
    The memory type should be specified in the board file using
    mach/ion.h. The ion platform driver will be responsible for
    allocating the correct memory.
    
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    
    Conflicts:
    
    	arch/arm/mach-msm/devices.h

--- arch/arm/mach-msm/devices.h
+++ arch/arm/mach-msm/devices.h
@@ -106,4 +106,8 @@ void __init msm_fb_register_device(char *name, void *data);
 void __init msm_camera_register_device(void *, uint32_t, void *);
 extern struct platform_device msm_device_touchscreen;
 
+extern struct pil_device peripheral_dsps;
+extern struct platform_device led_pdev;
+
+extern struct platform_device ion_dev;
 #endif
