ee750e811737bb3d4995dcf865fd28968814efec
kuogee hsieh <khsieh@codeaurora.org>
2011-09-02 09:44:22 -0700

    msm_fb: display: Add panel vertical porches into TE logic
    Panel vertical porches are not considered into tear check logic
    which cause TE can not tolerate panel vsync frequency variation
    after panel heating up. This cause mdp to hang up and no screen
    updated after that. This patch will have panel vertical porches
    considered in during configure TE registers.
    
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/mipi_novatek_cmd_qhd_pt.c

--- drivers/video/msm/mipi_novatek_cmd_qhd_pt.c
+++ drivers/video/msm/mipi_novatek_cmd_qhd_pt.c
@@ -63,7 +63,14 @@ static int __init mipi_cmd_novatek_blue_qhd_pt_init(void)
 	pinfo.bl_max = 15;
 	pinfo.bl_min = 1;
 	pinfo.fb_num = 2;
-	pinfo.clk_rate = 500000000;
+	pinfo.clk_rate = 454000000;
+	pinfo.is_3d_panel = FB_TYPE_3D_PANEL;
+	pinfo.lcd.vsync_enable = TRUE;
+	pinfo.lcd.hw_vsync_mode = TRUE;
+	pinfo.lcd.refx100 = 6000; /* adjust refx100 to prevent tearing */
+	pinfo.lcd.v_back_porch = 11;
+	pinfo.lcd.v_front_porch = 10;
+	pinfo.lcd.v_pulse_width = 5;
 
 	pinfo.mipi.mode = DSI_CMD_MODE;
 	pinfo.mipi.dst_format = DSI_CMD_DST_FORMAT_RGB888;
