4a3cd7931a571f79487051ea98cef04dd506b9d7
Ravishangar Kalyanam <rkalya@codeaurora.org>
2011-09-12 12:45:29 -0700

    msm_fb: display: Set DSI Video mode dest type as DISPLAY_LCDC
    Set DSI Video mode dest type as DISPLAY_LCDC to return
    correct fps to userspace in reserved[4]
    
    CRs-Fixed: 302910
    Signed-off-by: Ravishangar Kalyanam <rkalya@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/msm_fb.c

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -279,6 +279,12 @@ static ssize_t msm_fb_msm_fb_type(struct device *dev,
 	case DTV_PANEL:
 		ret = snprintf(buf, PAGE_SIZE, "dtv panel\n");
 		break;
+	case MIPI_VIDEO_PANEL:
+		ret = snprintf(buf, PAGE_SIZE, "mipi dsi video panel\n");
+		break;
+	case MIPI_CMD_PANEL:
+		ret = snprintf(buf, PAGE_SIZE, "mipi dsi cmd panel\n");
+		break;
 	default:
 		ret = snprintf(buf, PAGE_SIZE, "unknown panel\n");
 		break;
@@ -1106,6 +1112,25 @@ static int msm_fb_register(struct msm_fb_data_type *mfd)
 	var->xres_virtual = panel_info->xres;
 	var->yres_virtual = panel_info->yres * mfd->fb_page;
 	var->bits_per_pixel = bpp * 8;	/* FrameBuffer color depth */
+	if (mfd->dest == DISPLAY_LCD) {
+		var->reserved[4] = panel_info->lcd.refx100 / 100;
+	} else {
+		if (panel_info->type == MIPI_VIDEO_PANEL) {
+			var->reserved[4] = panel_info->mipi.frame_rate;
+		} else {
+			var->reserved[4] = panel_info->clk_rate /
+				((panel_info->lcdc.h_back_porch +
+				  panel_info->lcdc.h_front_porch +
+				  panel_info->lcdc.h_pulse_width +
+				  panel_info->xres) *
+				 (panel_info->lcdc.v_back_porch +
+				  panel_info->lcdc.v_front_porch +
+				  panel_info->lcdc.v_pulse_width +
+				  panel_info->yres));
+		}
+	}
+	pr_debug("reserved[4] %u\n", var->reserved[4]);
+
 		/*
 		 * id field for fb app
 		 */
