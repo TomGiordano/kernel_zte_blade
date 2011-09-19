b79f3664c3d058aebaa1812fab5b64d66b2954d3
Tom Giordano <tomgiordano83@gmail.com>
2011-09-20 07:48:47 +1000

    blade: update config

--- drivers/power/msm_battery.c
+++ drivers/power/msm_battery.c
@@ -88,7 +88,7 @@ static int wl_initialized = 0;//The rpc occur anytime ,so ,we must make sure tha
 
 
 /* liu.yijian@zte.com.cn 20091127 _FILE_INDEX */
-#define MSM_BATTERY_DEBUGX
+//#define MSM_BATTERY_DEBUGX
 #ifdef MSM_BATTERY_DEBUGX
 #define DEBUG_MSM_BATTERY(fmt, args...)\
     do\
