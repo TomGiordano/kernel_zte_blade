593ff6f85db867e5b480bdf410f36c51b49f6c3f
Nagamalleswararao Ganji <nganji@codeaurora.org>
2011-10-14 02:02:33 -0700

    msmfb: display: support for overlay wait ioctl
    this adds supports for overlay wait ioctl and it allows the
    userapce start overlay play and HDMI and can call wait
    later point of the time
    
    Change-Id: I8000cc52a497861e0a5fdfe58c0c049219724ab6
    Signed-off-by: Nagamalleswararao Ganji <nganji@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/msm_fb.h
    	include/linux/msm_mdp.h

--- drivers/video/msm/msm_fb.h
+++ drivers/video/msm/msm_fb.h
@@ -198,6 +198,10 @@ struct msm_fb_data_type {
 
 	struct clk *ebi1_clk;
 	boolean dma_update_flag;
+	struct timer_list msmfb_no_update_notify_timer;
+	struct completion msmfb_update_notify;
+	struct completion msmfb_no_update_notify;
+	u32 ov_start, ov_end;
 };
 
 struct dentry *msm_fb_get_debugfs_root(void);
