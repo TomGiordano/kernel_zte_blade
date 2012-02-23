67c6e91b8733f467c5a38a7dfca2bd58f69c7816
John Kenney <johnkenney02@gmail.com>
2012-02-23 02:33:40 +0000

    Increase max gpu speed Was set to 160mhz, set to max axi clock speed (200mhz)

--- arch/arm/mach-msm/board-zte-blade.c
+++ arch/arm/mach-msm/board-zte-blade.c
@@ -3667,7 +3667,7 @@ static void __init msm7x2x_init(void)
 	/* OEMs may modify the value at their discretion for performance */
 	/* The appropriate maximum replacement for 160000 is: */
 	/* clk_get_max_axi_khz() */
-	kgsl_pdata.high_axi_3d = 160000;
+	kgsl_pdata.high_axi_3d = clk_get_max_axi_khz();
 
 	/* 7x27 doesn't allow graphics clocks to be run asynchronously to */
 	/* the AXI bus */
