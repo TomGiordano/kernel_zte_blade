2d2bd936709d7d875cc85a65ce54d650ab733718
Tom Giordano <tomgiordano83@gmail.com>
2012-03-31 10:00:03 +1100

    fix-up

--- drivers/video/msm/mdp.h
+++ drivers/video/msm/mdp.h
@@ -55,6 +55,8 @@
 
 extern uint32 mdp_hw_revision;
 extern ulong mdp4_display_intf;
+extern spinlock_t mdp_spin_lock;
+extern int mdp_rev;
 
 #define MDP4_REVISION_V1		0
 #define MDP4_REVISION_V2		1
--- drivers/video/msm/msm_fb_panel.h
+++ drivers/video/msm/msm_fb_panel.h
@@ -135,6 +135,7 @@ struct mipi_panel_info {
 	char eof_bllp_power_stop;
 	char bllp_power_stop;
 	char traffic_mode;
+        char frame_rate;
 	/* command mode */
 	char interleave_max;
 	char insert_dcs_cmd;
